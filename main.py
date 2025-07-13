2eec26fc85cc2192bcef14944624486f3e4537cd
Liavvhen <liavh456@gmail.com>
2025-07-12 23:33:38 -0400

    refactor: use lifespan event to create tables

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from database import engine
 from models import Base
 
-app = FastAPI()
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("⏳ Creating tables...")
     Base.metadata.create_all(bind=engine)
+    yield  # App runs after this
+    print("🛑 Shutting down...")  # Runs on app shutdown
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
-def read_root():
-    return {"message": "🚀 Your D2C Growth AI is running!"}
+def root():
+    return {"message": "App is working"}
